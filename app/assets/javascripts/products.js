12d7753b1ed49511024665c774b49675c9ca654d
cntoplolicon <cntoplolicon@gmail.com>
2015-06-23 23:15:52 +0000

    upcoming products

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -5,4 +5,14 @@ $(document).ready(function() {
   $('.on-sale-products').masonry({
     itemSelector: '.on-sale-product'
   });
+
+  $('.trailer-panel').hide();
+  $('.upcoming-product').hover(function() {
+    $(this).find('.brand-image').hide();
+    $(this).find('.trailer-panel').show();
+  }, function() {
+    $(this).find('.brand-image').show();
+    $(this).find('.trailer-panel').hide();
+  });
 });
+
