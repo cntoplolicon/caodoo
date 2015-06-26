1cc4395332a67edad1fe788da3b054225bfa8c4c
cntoplolicon <cntoplolicon@gmail.com>
2015-06-26 20:56:46 +0800

    tweak product pages in IE7

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,17 +3,17 @@
 
 (function (window, $) {
   $(document).ready(function() {
-    //$('.on-sale-products').masonry({
-      //itemSelector: '.on-sale-product'
-    //});
+    $('.on-sale-products').masonry({
+      itemSelector: '.on-sale-product'
+    });
 
-    $('.trailer-panel').hide();
+    $('.trailer-image, .trailer-content').hide();
     $('.upcoming-product').hover(function() {
       $(this).find('.brand-image').hide();
-      $(this).find('.trailer-panel').show();
+      $(this).find('.trailer-image, .trailer-content').show();
     }, function() {
       $(this).find('.brand-image').show();
-      $(this).find('.trailer-panel').hide();
+      $(this).find('.trailer-image, .trailer-content').hide();
     });
 
     var countDown =  function() {
