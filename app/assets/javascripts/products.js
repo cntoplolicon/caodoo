c0dbbafdc5d9d78abb9ce4f3022996478e676941
cntoplolicon <cntoplolicon@gmail.com>
2015-06-27 15:00:04 +0800

    refactor hovering effect

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -7,15 +7,6 @@
       itemSelector: '.on-sale-product'
     });
 
-    $('.trailer-image, .trailer-content').hide();
-    $('.upcoming-product').hover(function() {
-      $(this).find('.brand-image').hide();
-      $(this).find('.trailer-image, .trailer-content').show();
-    }, function() {
-      $(this).find('.brand-image').show();
-      $(this).find('.trailer-image, .trailer-content').hide();
-    });
-
     var countDown =  function() {
       $('.product-sale-remain-time').each(function() {
         var time = $(this).data('time');
--- app/assets/javascripts/standalone.js
+++ app/assets/javascripts/standalone.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+    $(".about_segment_img_normal").hover(function(){
+      $(this).hide();
+      $(this).next().show();
+    });
+    $(".about_segment_img").mouseout(function(){
+      $(this).hide();
+      $(this).prev().show();
+    });
+  });
+
