8faa8e17f7b90d69c17249faf2166ced3b119ecb
jiewei1988 <499155526@qq.com>
2015-06-27 20:35:31 +0800

    format time showing in products page

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -11,12 +11,29 @@
       $('.product-sale-remain-time').each(function() {
         var time = $(this).data('time');
         time = Math.max(time - 1, 0);
+        var remainTimeString = showRemainTime(time);
         $(this).data('time', time);
-        $(this).text(time);
+        $(this).text(remainTimeString);
       });
     };
-    window.setInterval(countDown, 1000);
-    countDown();
+
+    if ($('.product-sale-remain-time').length > 0) {
+      countDown();
+      window.setInterval(countDown, 1000);
+    };
+
+    var productDetailCountDown = function() {
+        var time = $('.hidden').val();
+        time = Math.max(time - 1, 0);
+        var remainTimeString = showRemainTime(time);
+        $('.hidden').val(time);
+        $('.show-remain-time').text(remainTimeString);
+    };
+
+    if ($('.product-detail-countdown').length > 0) {
+      productDetailCountDown();
+      window.setInterval(productDetailCountDown, 1000);
+    };
 
     $('.product-detail-thumbnail').click(function() {
       $('.carousel-image').attr('src', $(this).attr('src'));
