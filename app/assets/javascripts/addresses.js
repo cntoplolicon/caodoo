88876e46f6ca3841ff492adc627bc00a619d9ba8
cntoplolicon <cntoplolicon@gmail.com>
2015-06-29 12:33:12 +0800

    delete address and set default address

--- app/assets/javascripts/addresses.js
+++ app/assets/javascripts/addresses.js
@@ -3,6 +3,27 @@
 
 $(document).ready(function () {
   $('.delete_address_action').click(function() {
-    $("#.
+    $('#delete_address_box').data('addressId', $(this).data('addressId'));
+    $('#delete_address_box').show();
+  });
+  $('.cancel_delete_address_button').click(function() {
+    $('#delete_address_box').hide();
+  });
+  $('.confirm_delete_address_button').click(function() {
+    var address_id = $('#delete_address_box').data('addressId');
+    $('.delete_address_' + address_id + '_form').submit();
+  });
+
+  $('.update_default_address_action').click(function() {
+    $('#update_default_address_box').data('addressId', $(this).data('addressId'));
+    $('#update_default_address_box').show();
+  });
+
+  $('.cancel_update_default_address_button').click(function() {
+    $('#update_default_address_box').hide();
+  });
+  $('.confirm_update_default_address_button').click(function() {
+    var address_id = $('#update_default_address_box').data('addressId');
+    $('.update_default_address_' + address_id + '_form').submit();
   });
 });
--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,9 +1,32 @@
 class AddressesController < ApplicationController
-  before_action :find_user
+  before_action :find_user, except: [:new]
 
   def index
-    @addresses = @user.addresses
+    @addresses = @user.addresses.where(deleted: false).order(created_at: :desc)
     render layout: 'account_setting'
+  end
+
+  def new
+    @address = Address.new
+  end
+
+  def edit
+    @address = @user.addresses.find(params[:id])
+  end
+
+  def update
+    Address.transaction do
+      @address = @user.addresses.find(params[:id])
+      @address.attributes = address_params
+      if @address.save
+        if @address.default
+          @user.addresses.where(deleted: false).where.not(id: @address.id).update_all(default: false)
+        end
+        redirect_to action: :index 
+      else
+        render 'edit'
+      end
+    end
   end
 
   private
@@ -12,5 +35,9 @@ class AddressesController < ApplicationController
     user_id = params[:user_id].to_i
     head :forbidden if user_id != session[:login_user_id]
     @user = User.find(user_id)
+  end
+
+  def address_params
+    params.require(:address).permit(:user_id, :receiver, :phone, :province_code, :city_code, :district_code, :detailed_address, :default, :deleted)
   end
 end
