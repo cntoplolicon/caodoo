2b8ce3eb4e047809f1d6619827e9b82351de3b08
cntoplolicon <cntoplolicon@gmail.com>
2015-07-05 21:08:05 +0800

    contest products qrcode page

--- app/controllers/contest_teams_controller.rb
+++ app/controllers/contest_teams_controller.rb
@@ -77,6 +77,8 @@ class ContestTeamsController < ApplicationController
       .includes(product_view: :product_carousel_images)
       .where(product_table[:contest_level].gteq(@contest_team.level))
       .order(priority: :desc)
+    @links = @products.map { |product| {title: product.name, url: contest_team_contest_product_url(@contest_team.identifier, product)} }
+    @links.unshift({title: '大赛活动页', url: contest_team_contest_products_url(@contest_team)})
   end
 
   def notification
