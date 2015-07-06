9d3a31423fff51a09ffff271295f3c47854efdf7
jiewei1988 <499155526@qq.com>
2015-07-06 16:35:54 +0800

    Fix mobile masonry issue

--- app/assets/javascripts/mobile/products.js
+++ app/assets/javascripts/mobile/products.js
@@ -3,9 +3,11 @@
 
 (function(window, $) {
   $(document).ready(function() {
-    new Masonry(document.querySelector('.on-sale-products'), {
-      itemSelector: '.on-sale-product'
-    });
+  	if ($('.on-sale-products').length > 0) {
+  	  new Masonry(document.querySelector('.on-sale-products'), {
+  	    itemSelector: '.on-sale-product'
+  	  });
+  	}
 
     var countDown = function() {
       $('.product-sale-remain-time').each(function() {
