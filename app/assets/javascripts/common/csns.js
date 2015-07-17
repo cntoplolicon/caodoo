726e958d666c7dc6962da28475716fc88f07fc28
cntoplolicon <cntoplolicon@gmail.com>
2015-07-17 16:54:08 +0800

    implement social network sharing in contest product list page

---
+++ app/assets/javascripts/common/csns.js
@@ -0,0 +1,48 @@
+(function($) {
+  var platforms = {
+    'tsina': 'http://service.weibo.com/share/share.php?title={{title}}&url={{url}}&pic={{pic}}',
+    'qzone': 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?title={{title}}&desc={{desc}}&url={{url}}&pics={{pic}}',
+    'renren': 'http://widget.renren.com/dialog/share?resourceUrl={{url}}&title={{title}}&description={{desc}}&pic={{pic}}'
+  };
+  $.fn.csns = function(options) {
+    var settings = $.extend({
+      url: window.location.href,
+      title: document.title,
+      desc: document.title,
+      pic: $('img').prop('src'),
+      qrcode: {
+        render: 'div',
+        size: 185,
+        ecLevel: 'H',
+        minVersion: 5
+      },
+    }, options);
+    var wechatDialog;
+    if (this.find('.csns-share-button[data-cmd="wechat"]').length > 0) {
+      var wechatDialog = $('<div class="csns-wechat-dialog"><div class="csns-wechat-dialog-header"><span>分享到微信朋友圈</span><a href="#" class="">×</a></div><div class="csns-wechat-dialog-qrcode"></div><div class="csns-wechat-dialog-footer">打开微信，点击底部的“发现“<br>使用“扫一扫”即可将网页分享至朋友圈</div></div>');
+      wechatDialog.find('.csns-wechat-dialog-qrcode').qrcode(settings.qrcode);
+      $('body').append(wechatDialog);
+      wechatDialog.find('.csns-wechat-dialog-header a').click(function(event) {
+        event.preventDefault();
+        wechatDialog.hide();
+      });
+    }
+    this.find('.csns-share-button').click(function(event) {
+      event.preventDefault();
+      var command = $(this).data('cmd');
+      if (command === 'wechat') {
+        wechatDialog.css('top', ($(window).height() - wechatDialog.height()) / 2 + $(window).scrollTop());
+        wechatDialog.css('left', ($(window).width() - wechatDialog.width()) / 2 + $(window).scrollLeft());
+        wechatDialog.show();
+      } else {
+        var platformUrl = platforms[command];
+        if (platformUrl !== undefined) {
+          for (var key in settings) {
+            platformUrl = platformUrl.replace('{{' + key + '}}', encodeURIComponent(settings[key]));
+          }
+        }
+        window.open(platformUrl);
+      }
+    });
+  }
+})(jQuery);
--- app/assets/javascripts/desktop/contest_products.js
+++ app/assets/javascripts/desktop/contest_products.js
@@ -1,17 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
-  if ($('.game_share_content').length > 0) {
-    window._bd_share_config = {
-      common: {
-        bdText: '我们参加了大学生营销策划赛，来围观吧~',
-        bdDesc: '我们参加了大学生营销策划赛，来围观吧~',
-        bdPic: $('.game_poster')[0].src
-      },
-      share: [{
-        "bdSize": 24
-      }]
-    }
-    with(document) 0[(getElementsByTagName('head')[0] || body).appendChild(createElement('script')).src = 'http://bdimg.share.baidu.com/static/api/js/share.js?cdnversion=' + ~(-new Date() / 36e5)];
-  }
+  $('.game_share_content').csns({
+    title: '我们参加了大学生营销策划赛，来围观吧~',
+    desc: '我们参加了大学生营销策划赛，来围观吧~',
+    pic: $('.game_poster')[0].src
+  });
 });
