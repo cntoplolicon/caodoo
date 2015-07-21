39498bf68c4fbdd1749c82ed7ea8fc5a2f06eb1e
cntoplolicon <cntoplolicon@gmail.com>
2015-07-21 17:05:46 +0800

    fix bugs in refund record js

--- app/assets/javascripts/desktop/refund_records.js
+++ app/assets/javascripts/desktop/refund_records.js
@@ -1,21 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(function() {
+  var setupRefundRecordForm = function() {
+    $('input').placeholder();
+    $('#refund_record_express_id').selectlist({
+      zIndex:20,
+      width:180,
+      height:32,
+    });
+  }
+
   $('.refund_record_new_action, .refund_record_edit_action').click(function(event) {
     event.preventDefault();
     $('#team_return_goods_popup').load($(this).attr('href'), function() {
-      $('input, textarea').placeholder();
       $('#team_return_goods_popup').show();
-      $('input').placeholder();
-
-      $('#refund_record_express_id').selectlist({
-        zIndex:20,
-        width:180,
-        height:32,
-        onChange:function(){
-        }
-      })
-
+      setupRefundRecordForm();
     });
   });
   $('#team_return_goods_popup').on('click', '.return_good_reset', function() {
@@ -23,7 +22,7 @@ $(function() {
   });
   $('#team_return_goods_popup').on('click', '.return_good_submit', function(event) {
     event.preventDefault();
-    if (validate_address()) {
+    if (validate_refund_record()) {
       var form = $(this).closest('form');
       $.ajax({
         url: form.attr('action'),
@@ -32,7 +31,7 @@ $(function() {
         statusCode: {
           400: function(xhr) {
             $('#team_return_goods_popup').html(xhr.responseText);
-            $('input').placeholder();
+            setupRefundRecordForm();
           },
           302: function() {
             window.location.reload();
@@ -55,7 +54,7 @@ $(document).ready(function() {
   });
 });
 
-function validate_address() {
+function validate_refund_record() {
   var b = true;
   if ($("#refund_record_order_number").val() == "") {
     $("#refund_record_order_number_error").text(validate_message.order_number.blank);
@@ -93,5 +92,4 @@ function validate_address() {
     $("#refund_record_order_count_error").text("");
   }
   return b;
-
 }
