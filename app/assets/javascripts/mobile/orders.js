6d24d21c4c3bb8ec267dde8edb294fbd69d0b086
mengweidong <97568260@qq.com>
2015-07-27 18:03:49 +0800

    mobile add remark

--- app/assets/javascripts/mobile/orders.js
+++ app/assets/javascripts/mobile/orders.js
@@ -121,4 +121,10 @@ $(document).ready(function() {
       });
     }, 7000);
   }
+  $("#new_order").submit(function(){
+    if($("#order_remark").val().length>255){
+      $("#order-remark-error").text("补充说明太长啦,请简述!");
+      return false;
+    }
+  })
 });
