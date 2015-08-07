e2ce1c3157efc574a24424b3f18e922362feb033
cntoplolicon <cntoplolicon@gmail.com>
2015-08-07 14:41:44 +0800

    show product version in order submission page

--- app/assets/javascripts/mobile/orders.js
+++ app/assets/javascripts/mobile/orders.js
@@ -121,10 +121,4 @@ $(document).ready(function() {
       });
     }, 7000);
   }
-  $("#new_order").submit(function(){
-    if($("#order_remark").val().length>255){
-      $("#order-remark-error").text("补充说明太长啦,请简述!");
-      return false;
-    }
-  })
 });
