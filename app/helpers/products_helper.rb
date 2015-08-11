05c2dea68ff797f56532918dbe8130a8945b82e5
cntoplolicon <cntoplolicon@gmail.com>
2015-08-11 12:12:32 +0800

    fix cache key

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,7 +2,7 @@ module ProductsHelper
   def products_signature
     keys = [Product.count, Product.maximum(:updated_at).utc.iso8601,
             Brand.count, Brand.maximum(:updated_at).utc.iso8601,
-            ProductGroup.count, Brand.maximum(:updated_at).utc.iso8601,
+            ProductGroup.count, ProductGroup.maximum(:updated_at).utc.iso8601,
             Time.now.utc.change(sec: 0).iso8601]
     Digest::MD5.hexdigest(keys.join)
   end
